1548ae0b7f2696c03b9bc21b3309d871d0c6fd04
Jonny Burger <jonathanburger11@gmail.com>
2020-09-13 22:10:57 +0200

    chore(release): example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,16 +1,24 @@
 import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
 import AnimateableText from 'react-native-animateable-text';
-import Animated, { concat } from 'react-native-reanimated';
+import Animated, { concat, round } from 'react-native-reanimated';
+import { withTimingTransition } from 'react-native-redash';
 
 const AnimatedText = Animated.createAnimatedComponent(AnimateableText);
 
 export default function App() {
-  const value = React.useMemo(() => new Animated.Value(10), []);
-  const str = React.useMemo(() => concat('Animated ', value), [value]);
+  const value = React.useMemo(() => new Animated.Value(1000), []);
+  const str = React.useMemo(
+    () =>
+      concat(
+        'Animated ',
+        round(withTimingTransition(value, { duration: 1000 }))
+      ),
+    [value]
+  );
   return (
     <View style={styles.container}>
-      <AnimatedText text={str}></AnimatedText>
+      <AnimatedText text={str} />
     </View>
   );
 }
