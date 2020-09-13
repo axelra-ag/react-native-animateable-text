743b6ebc92236891c183d0f0bf4f5f421e03f39f
Jonny Burger <jonathanburger11@gmail.com>
2020-09-13 17:29:19 +0200

    feat(feat): rename input to text

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { AnimatedInput } from 'react-native-reanimated-text';
+import { AnimatedText } from 'react-native-reanimated-text';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <AnimatedInput actualText="hi"></AnimatedInput>
+      <AnimatedText actualText="hi"></AnimatedText>
     </View>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import { NativeModules } from 'react-native';
-const Input = require('./Input');
+export const AnimatedText = require('./AnimatedText');
 type ReanimatedTextType = {
   multiply(a: number, b: number): Promise<number>;
 };
 
 const { ReanimatedText } = NativeModules;
-
-export const AnimatedInput = Input;
 
 export default ReanimatedText as ReanimatedTextType;
