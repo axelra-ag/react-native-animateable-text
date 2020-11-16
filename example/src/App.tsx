ff2d4093420a8a73cb0fbb22ad513c21f840773c
Jonny Burger <jonathanburger11@gmail.com>
2020-11-16 19:14:09 +0100

    feat: update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,32 +1,78 @@
 import * as React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import AnimateableText from 'react-native-animateable-text';
-import Animated, { concat, round } from 'react-native-reanimated';
-import { withTimingTransition } from 'react-native-redash';
+import { PanGestureHandler } from 'react-native-gesture-handler';
+import Animated, { concat, multiply, round } from 'react-native-reanimated';
+import { ReText, usePanGestureHandler } from 'react-native-redash';
 
-const AnimatedText = Animated.createAnimatedComponent(AnimateableText);
+const AnimatedText = Animated.createAnimatedComponent(
+  AnimateableText
+) as typeof AnimateableText;
 
-export default function App() {
-  const value = React.useMemo(() => new Animated.Value(1000), []);
-  const str = React.useMemo(
-    () =>
-      concat(
-        'Animated ',
-        round(withTimingTransition(value, { duration: 1000 }))
-      ),
-    [value]
-  );
-  return (
-    <View style={styles.container}>
-      <AnimatedText text={str} />
-    </View>
-  );
-}
+const style = {
+  fontSize: 30,
+  fontWeight: 'bold' as const,
+  color: 'black' as const,
+  paddingTop: 0,
+  paddingBottom: 0,
+};
 
 const styles = StyleSheet.create({
+  flex1: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  row: { flexDirection: 'row', paddingLeft: 20, paddingRight: 20 },
+  spacer: {
+    height: 20,
+  },
+  outerBar: {
+    width: '90%',
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    height: 40,
+  },
 });
+
+export default function App() {
+  const panGestureHandler = usePanGestureHandler();
+  const str = React.useMemo(
+    () => concat(round(multiply(panGestureHandler.translation.x, 1000)), '×'),
+    [panGestureHandler.translation.x]
+  );
+
+  const bar = React.useMemo(() => {
+    return {
+      left: panGestureHandler.position.x,
+      height: 40,
+      width: 10,
+      backgroundColor: 'black',
+    };
+  }, [panGestureHandler.position.x]);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <Text style={style}>ReText: </Text>
+        <View style={styles.flex1} />
+        <ReText text={str} style={style} />
+      </View>
+      <View style={styles.row}>
+        <Text style={style} selectable>
+          AnimateableText:{' '}
+        </Text>
+        <View style={styles.flex1} />
+        <AnimatedText selectable text={str} style={style} />
+      </View>
+      <View style={styles.spacer} />
+      <PanGestureHandler {...panGestureHandler.gestureHandler}>
+        <Animated.View style={styles.outerBar}>
+          <Animated.View style={bar} />
+        </Animated.View>
+      </PanGestureHandler>
+    </View>
+  );
+}
