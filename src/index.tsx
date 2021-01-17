eff488558739f1c7f9fa3effb2036037ded2bddf
Oliver Winter <owinter86@gmail.com>
2021-01-17 18:14:47 +0100

    Add text to reanimated white listed props to wor
    Fixes issue https://github.com/axelra-ag/react-native-animateable-text/issues/4

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import Animated from 'react-native-reanimated';
 import { AnimateableText as RawAnimateableText } from './AnimateableText';
 
+Animated.addWhitelistedNativeProps({ text: true });
+
 const AnimateableText = Animated.createAnimatedComponent(
   RawAnimateableText
 ) as typeof RawAnimateableText;
