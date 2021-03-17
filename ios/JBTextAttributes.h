b8a50138778e2ed6af80dbc8b2ac14df44d158ce
Jonny Burger <jonathanburger11@gmail.com>
2021-03-17 12:45:52 +0100

    fix(core): crash when changing styles

--- ios/JBTextAttributes.h
+++ ios/JBTextAttributes.h
@@ -54,7 +54,7 @@ extern NSString *const JBTextAttributesTagAttributeName;
 @property (nonatomic, strong, nullable) NSNumber *tag;
 @property (nonatomic, assign) UIUserInterfaceLayoutDirection layoutDirection;
 @property (nonatomic, assign) RCTTextTransform textTransform;
-@property (nonatomic, assign) NSString *text;
+@property (nonatomic, copy, nullable) NSString *text;
 
 #pragma mark - Inheritance
 
