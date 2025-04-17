0c731dd6c3b4c3df1bc77d53361a044288152b6c
Wadah Esam <wadah-wleed@hotmail.com>
2025-04-17 16:30:05 +0300

    chore: run (prepare)

---
+++ src/specs/JBAnimatedTextNativeComponent.js
@@ -0,0 +1,5 @@
+/*
+ * Custom -> used only for codegen
+ */
+import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
+export default codegenNativeComponent('JBAnimatedText');
