4d6400ebffc64b989588b772bd6e80abfa661a8b
mzaien <xa300600@hotmail.com>
2026-06-04 14:52:30 +0300

    chore: remove duplicate non needed js code

--- src/specs/JBAnimatedTextNativeComponent.js
+++
@@ -1,5 +0,0 @@
-/*
- * Custom -> used only for codegen
- */
-import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
-export default codegenNativeComponent('JBAnimatedText');
